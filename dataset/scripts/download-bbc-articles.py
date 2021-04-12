7ac5a3ce7d75c17b355726570f10dbc056d8fb9e
Ramachandren Shankar <43037428+ramshankar99@users.noreply.github.com>
2021-04-12 20:24:57 +0530

    debugged parse

--- dataset/scripts/download-bbc-articles.py
+++ dataset/scripts/download-bbc-articles.py
@@ -158,7 +158,7 @@ def DownloadMode(urls_file, missing_urls_file, downloads_dir, request_parallelis
 	print('Downloading URLs from the %s file:' % urls_file)
 	urls_full = ReadUrls(urls_file)
 	
-	urls_valid_todownload = urls_full[:100]
+	urls_valid_todownload = urls_full[:]
 	missing_urls_filename = missing_urls_file
 	if os.path.exists(missing_urls_filename):
 		print('Only downloading missing URLs')
@@ -172,7 +172,7 @@ def DownloadMode(urls_file, missing_urls_file, downloads_dir, request_parallelis
 	
 	while(len(urls_left_todownload) != 0):
 		
-		with Pool(5) as pool:
+		with Pool(20) as pool:
 			results = pool.map(DownloadMapper, urls_left_todownload)
 			progress_bar = ProgressBar(len(urls_left_todownload))
 
